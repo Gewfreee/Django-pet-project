d71c596c8abdfff6cda3f9f7743575db865be1b2
Moonlightwas <Alexandrkumaneev@mail.ru>
2025-02-14 14:09:57 +0500

    Update urls.py
    api urls updated

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('library.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
